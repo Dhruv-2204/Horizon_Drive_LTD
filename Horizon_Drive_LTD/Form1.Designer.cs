7f12e1085a802926f3fab8c1e0af342dbe4e0acc
Dhruv Ramachundrah <DR717@live.mdx.ac.uk>
2025-02-24 15:06:44 +0400

    First shot at GUI

--- Horizon_Drive_LTD/Form1.Designer.cs
+++ Horizon_Drive_LTD/Form1.Designer.cs
@@ -28,18 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            splitContainer1 = new SplitContainer();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.SuspendLayout();
             SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Dock = DockStyle.Fill;
+            splitContainer1.Location = new Point(0, 0);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.AccessibleName = "";
+            splitContainer1.Panel1.BackColor = Color.DarkCyan;
+            splitContainer1.Panel1.Controls.Add(button3);
+            splitContainer1.Panel1.Controls.Add(button2);
+            splitContainer1.Panel1.Controls.Add(button1);
+            splitContainer1.Panel1.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            splitContainer1.Panel1.ForeColor = Color.Coral;
+            splitContainer1.Panel1.Paint += splitContainer1_Panel1_Paint;
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.BackColor = SystemColors.ActiveCaptionText;
+            splitContainer1.Panel2.BackgroundImage = (Image)resources.GetObject("splitContainer1.Panel2.BackgroundImage");
+            splitContainer1.Panel2.BackgroundImageLayout = ImageLayout.Zoom;
+            splitContainer1.Panel2.Paint += splitContainer1_Panel2_Paint;
+            splitContainer1.Size = new Size(1218, 565);
+            splitContainer1.SplitterDistance = 406;
+            splitContainer1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.PaleTurquoise;
+            button1.Location = new Point(110, 89);
+            button1.Name = "button1";
+            button1.Size = new Size(186, 87);
+            button1.TabIndex = 0;
+            button1.Text = "Home";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.PaleTurquoise;
+            button2.Location = new Point(110, 239);
+            button2.Name = "button2";
+            button2.Size = new Size(186, 87);
+            button2.TabIndex = 1;
+            button2.Text = "Search ";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.PaleTurquoise;
+            button3.Location = new Point(110, 385);
+            button3.Name = "button3";
+            button3.Size = new Size(186, 87);
+            button3.TabIndex = 2;
+            button3.Text = "Profile";
+            button3.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(876, 508);
+            ClientSize = new Size(1218, 565);
+            Controls.Add(splitContainer1);
+            Cursor = Cursors.Cross;
             Name = "Form1";
-            Text = "Form1";
+            Text = "Horizon Drive LTD";
+            Load += Form1_Load;
+            splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
+
+        private SplitContainer splitContainer1;
+        private Button button1;
+        private Button button3;
+        private Button button2;
     }
 }
--- Horizon_Drive_LTD/Form1.cs
+++ Horizon_Drive_LTD/Form1.cs
@@ -6,5 +6,25 @@ namespace Horizon_Drive_LTD
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
