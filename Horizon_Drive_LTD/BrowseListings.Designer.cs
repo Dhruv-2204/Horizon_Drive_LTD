fa0f1b005b2ac97ae42a2113985200b154cd682b
Yash-Booputh <163565802+Yash-Booputh@users.noreply.github.com>
2025-03-08 20:04:46 +0400

    Beta 1.0

---
+++ Horizon_Drive_LTD/BrowseListings.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace Horizon_Drive_LTD
+{
+    partial class BrowseListings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelSidebar = new System.Windows.Forms.Panel();
+            this.btnOptions = new System.Windows.Forms.Button();
+            this.btnManageBooking = new System.Windows.Forms.Button();
+            this.btnListCar = new System.Windows.Forms.Button();
+            this.btnBrowseListings = new System.Windows.Forms.Button();
+            this.pictureBoxLogo = new System.Windows.Forms.PictureBox();
+            this.panelMain = new System.Windows.Forms.Panel();
+            this.panelContent = new System.Windows.Forms.Panel();
+            this.flowLayoutPanelListings = new System.Windows.Forms.FlowLayoutPanel();
+            this.panelHeader = new System.Windows.Forms.Panel();
+            this.buttonFilters = new System.Windows.Forms.Button();
+            this.panelSearch = new System.Windows.Forms.Panel();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.labelBrowseListings = new System.Windows.Forms.Label();
+            this.panelSidebar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).BeginInit();
+            this.panelMain.SuspendLayout();
+            this.panelContent.SuspendLayout();
+            this.panelHeader.SuspendLayout();
+            this.panelSearch.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelSidebar
+            // 
+            this.panelSidebar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(30)))), ((int)(((byte)(45)))));
+            this.panelSidebar.Controls.Add(this.btnOptions);
+            this.panelSidebar.Controls.Add(this.btnManageBooking);
+            this.panelSidebar.Controls.Add(this.btnListCar);
+            this.panelSidebar.Controls.Add(this.btnBrowseListings);
+            this.panelSidebar.Controls.Add(this.pictureBoxLogo);
+            this.panelSidebar.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelSidebar.Location = new System.Drawing.Point(0, 0);
+            this.panelSidebar.Name = "panelSidebar";
+            this.panelSidebar.Size = new System.Drawing.Size(250, 700);
+            this.panelSidebar.TabIndex = 0;
+            // 
+            // btnOptions
+            // 
+            this.btnOptions.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(85)))), ((int)(((byte)(110)))));
+            this.btnOptions.FlatAppearance.BorderSize = 0;
+            this.btnOptions.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOptions.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOptions.ForeColor = System.Drawing.Color.White;
+            this.btnOptions.Location = new System.Drawing.Point(40, 516);
+            this.btnOptions.Name = "btnOptions";
+            this.btnOptions.Size = new System.Drawing.Size(170, 50);
+            this.btnOptions.TabIndex = 4;
+            this.btnOptions.Text = "Options";
+            this.btnOptions.UseVisualStyleBackColor = false;
+            this.btnOptions.Click += new System.EventHandler(this.btnOptions_Click);
+            // 
+            // btnManageBooking
+            // 
+            this.btnManageBooking.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(85)))), ((int)(((byte)(110)))));
+            this.btnManageBooking.FlatAppearance.BorderSize = 0;
+            this.btnManageBooking.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnManageBooking.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnManageBooking.ForeColor = System.Drawing.Color.White;
+            this.btnManageBooking.Location = new System.Drawing.Point(40, 446);
+            this.btnManageBooking.Name = "btnManageBooking";
+            this.btnManageBooking.Size = new System.Drawing.Size(170, 50);
+            this.btnManageBooking.TabIndex = 3;
+            this.btnManageBooking.Text = "Manage Booking";
+            this.btnManageBooking.UseVisualStyleBackColor = false;
+            this.btnManageBooking.Click += new System.EventHandler(this.btnManageBooking_Click);
+            // 
+            // btnListCar
+            // 
+            this.btnListCar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(85)))), ((int)(((byte)(110)))));
+            this.btnListCar.FlatAppearance.BorderSize = 0;
+            this.btnListCar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnListCar.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnListCar.ForeColor = System.Drawing.Color.White;
+            this.btnListCar.Location = new System.Drawing.Point(40, 376);
+            this.btnListCar.Name = "btnListCar";
+            this.btnListCar.Size = new System.Drawing.Size(170, 50);
+            this.btnListCar.TabIndex = 2;
+            this.btnListCar.Text = "List a car";
+            this.btnListCar.UseVisualStyleBackColor = false;
+            this.btnListCar.Click += new System.EventHandler(this.btnListCar_Click);
+            // 
+            // btnBrowseListings
+            // 
+            this.btnBrowseListings.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(173)))), ((int)(((byte)(216)))), ((int)(((byte)(230)))));
+            this.btnBrowseListings.FlatAppearance.BorderSize = 0;
+            this.btnBrowseListings.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBrowseListings.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBrowseListings.ForeColor = System.Drawing.Color.Black;
+            this.btnBrowseListings.Location = new System.Drawing.Point(40, 306);
+            this.btnBrowseListings.Name = "btnBrowseListings";
+            this.btnBrowseListings.Size = new System.Drawing.Size(170, 50);
+            this.btnBrowseListings.TabIndex = 1;
+            this.btnBrowseListings.Text = "Browse listings";
+            this.btnBrowseListings.UseVisualStyleBackColor = false;
+            this.btnBrowseListings.Click += new System.EventHandler(this.btnBrowseListings_Click);
+            // 
+            // pictureBoxLogo
+            // 
+            this.pictureBoxLogo.Location = new System.Drawing.Point(50, 50);
+            this.pictureBoxLogo.Name = "pictureBoxLogo";
+            this.pictureBoxLogo.Size = new System.Drawing.Size(150, 150);
+            this.pictureBoxLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxLogo.TabIndex = 0;
+            this.pictureBoxLogo.TabStop = false;
+            // 
+            // panelMain
+            // 
+            this.panelMain.BackColor = System.Drawing.Color.White;
+            this.panelMain.Controls.Add(this.panelContent);
+            this.panelMain.Controls.Add(this.panelHeader);
+            this.panelMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelMain.Location = new System.Drawing.Point(250, 0);
+            this.panelMain.Name = "panelMain";
+            this.panelMain.Size = new System.Drawing.Size(950, 700);
+            this.panelMain.TabIndex = 1;
+            // 
+            // panelContent
+            // 
+            this.panelContent.Controls.Add(this.flowLayoutPanelListings);
+            this.panelContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelContent.Location = new System.Drawing.Point(0, 100);
+            this.panelContent.Name = "panelContent";
+            this.panelContent.Padding = new System.Windows.Forms.Padding(20);
+            this.panelContent.Size = new System.Drawing.Size(950, 600);
+            this.panelContent.TabIndex = 1;
+            // 
+            // flowLayoutPanelListings
+            // 
+            this.flowLayoutPanelListings.AutoScroll = true;
+            this.flowLayoutPanelListings.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanelListings.Location = new System.Drawing.Point(20, 20);
+            this.flowLayoutPanelListings.Name = "flowLayoutPanelListings";
+            this.flowLayoutPanelListings.Size = new System.Drawing.Size(910, 560);
+            this.flowLayoutPanelListings.TabIndex = 0;
+            // 
+            // panelHeader
+            // 
+            this.panelHeader.Controls.Add(this.buttonFilters);
+            this.panelHeader.Controls.Add(this.panelSearch);
+            this.panelHeader.Controls.Add(this.labelBrowseListings);
+            this.panelHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelHeader.Location = new System.Drawing.Point(0, 0);
+            this.panelHeader.Name = "panelHeader";
+            this.panelHeader.Size = new System.Drawing.Size(950, 100);
+            this.panelHeader.TabIndex = 0;
+            // 
+            // buttonFilters
+            // 
+            this.buttonFilters.BackColor = System.Drawing.Color.White;
+            this.buttonFilters.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.buttonFilters.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonFilters.Location = new System.Drawing.Point(826, 35);
+            this.buttonFilters.Name = "buttonFilters";
+            this.buttonFilters.Size = new System.Drawing.Size(40, 30);
+            this.buttonFilters.TabIndex = 2;
+            this.buttonFilters.Text = "≡";
+            this.buttonFilters.UseVisualStyleBackColor = false;
+            // 
+            // panelSearch
+            // 
+            this.panelSearch.BackColor = System.Drawing.Color.White;
+            this.panelSearch.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelSearch.Controls.Add(this.buttonSearch);
+            this.panelSearch.Controls.Add(this.textBoxSearch);
+            this.panelSearch.Location = new System.Drawing.Point(621, 35);
+            this.panelSearch.Name = "panelSearch";
+            this.panelSearch.Size = new System.Drawing.Size(200, 30);
+            this.panelSearch.TabIndex = 1;
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.BackColor = System.Drawing.Color.White;
+            this.buttonSearch.FlatAppearance.BorderSize = 0;
+            this.buttonSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSearch.Font = new System.Drawing.Font("Segoe MDL2 Assets", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSearch.Location = new System.Drawing.Point(168, 0);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(30, 28);
+            this.buttonSearch.TabIndex = 1;
+            this.buttonSearch.Text = "🔍";
+            this.buttonSearch.UseVisualStyleBackColor = false;
+            // 
+            // textBoxSearch
+            // 
+            this.textBoxSearch.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxSearch.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxSearch.Location = new System.Drawing.Point(5, 5);
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(160, 18);
+            this.textBoxSearch.TabIndex = 0;
+            this.textBoxSearch.TextChanged += new System.EventHandler(this.textBoxSearch_TextChanged);
+            // 
+            // labelBrowseListings
+            // 
+            this.labelBrowseListings.AutoSize = true;
+            this.labelBrowseListings.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelBrowseListings.Location = new System.Drawing.Point(35, 35);
+            this.labelBrowseListings.Name = "labelBrowseListings";
+            this.labelBrowseListings.Size = new System.Drawing.Size(187, 32);
+            this.labelBrowseListings.TabIndex = 0;
+            this.labelBrowseListings.Text = "Browse Listings";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 700);
+            this.Controls.Add(this.panelMain);
+            this.Controls.Add(this.panelSidebar);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Horizon Drive - Car Rental";
+            this.panelSidebar.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).EndInit();
+            this.panelMain.ResumeLayout(false);
+            this.panelContent.ResumeLayout(false);
+            this.panelHeader.ResumeLayout(false);
+            this.panelHeader.PerformLayout();
+            this.panelSearch.ResumeLayout(false);
+            this.panelSearch.PerformLayout();
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelSidebar;
+        private System.Windows.Forms.PictureBox pictureBoxLogo;
+        private System.Windows.Forms.Button btnOptions;
+        private System.Windows.Forms.Button btnManageBooking;
+        private System.Windows.Forms.Button btnListCar;
+        private System.Windows.Forms.Button btnBrowseListings;
+        private System.Windows.Forms.Panel panelMain;
+        private System.Windows.Forms.Panel panelHeader;
+        private System.Windows.Forms.Label labelBrowseListings;
+        private System.Windows.Forms.Panel panelContent;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanelListings;
+        private System.Windows.Forms.Panel panelSearch;
+        private System.Windows.Forms.TextBox textBoxSearch;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.Button buttonFilters;
+    }
+}
---
+++ Horizon_Drive_LTD/BrowseListings.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Horizon_Drive_LTD
+{
+    public partial class BrowseListings : Form
+    {
+        // List to store car listings
+        private List<CarListing> carListings = new List<CarListing>();
+
+        public BrowseListings()
+        {
+            InitializeComponent();
+            this.Size = new Size(1600, 900);
+            LoadCarListings();
+            PopulateCarListings();
+        }
+
+        private void LoadCarListings()
+        {
+            // Sample car data - in a real app, this would come from a database
+            carListings.Add(new CarListing
+            {
+                Id = 1,
+                Make = "Ford",
+                Model = "Raptor",
+                Year = 2023,
+                Description = "A powerful off-road pickup built for performance and adventure. Rent now for a rugged and thrilling ride!",
+                PricePerDay = 15000,
+                ImagePath = "ford_raptor.jpg"
+            });
+
+            carListings.Add(new CarListing
+            {
+                Id = 2,
+                Make = "BMW",
+                Model = "X4",
+                Year = 2023,
+                Description = "A luxury high-performance SUV with bold design and powerful hybrid technology. Rent now for an elite driving experience!",
+                PricePerDay = 30000,
+                ImagePath = "bmw_x4.jpg"
+            });
+
+            carListings.Add(new CarListing
+            {
+                Id = 3,
+                Make = "Corvette",
+                Model = "C8",
+                Year = 2023,
+                Description = "A sleek, mid-engine sports car with thrilling performance and sharp handling. Rent now for an unforgettable drive!",
+                PricePerDay = 20000,
+                ImagePath = "corvette_c8.jpg"
+            });
+
+            carListings.Add(new CarListing
+            {
+                Id = 4,
+                Make = "Ford",
+                Model = "Mustang",
+                Year = 2023,
+                Description = "A modern muscle car with bold design and powerful performance. Rent now for an exhilarating ride!",
+                PricePerDay = 25000,
+                ImagePath = "ford_mustang.jpg"
+            });
+
+            // Add more cars for the bottom row
+            carListings.Add(new CarListing
+            {
+                Id = 5,
+                Make = "Mercedes",
+                Model = "AMG",
+                Year = 2023,
+                Description = "Performance luxury at its finest with distinctive styling and remarkable power.",
+                PricePerDay = 28000,
+                ImagePath = "mercedes_amg.jpg"
+            });
+
+            carListings.Add(new CarListing
+            {
+                Id = 6,
+                Make = "Honda",
+                Model = "Civic",
+                Year = 2022,
+                Description = "Reliable, efficient and perfect for city driving with excellent fuel economy.",
+                PricePerDay = 8000,
+                ImagePath = "honda_civic.jpg"
+            });
+
+            carListings.Add(new CarListing
+            {
+                Id = 7,
+                Make = "Mercedes",
+                Model = "GLE Coupe",
+                Year = 2023,
+                Description = "Elegance meets performance in this luxury SUV coupe with cutting-edge technology.",
+                PricePerDay = 32000,
+                ImagePath = "mercedes_gle.jpg"
+            });
+
+            carListings.Add(new CarListing
+            {
+                Id = 8,
+                Make = "Mercedes",
+                Model = "AMG GT",
+                Year = 2023,
+                Description = "The pinnacle of Mercedes performance with breathtaking design and power.",
+                PricePerDay = 35000,
+                ImagePath = "mercedes_amg_gt.jpg"
+            });
+        }
+
+        private void PopulateCarListings()
+        {
+            // Clear the flowLayoutPanel first
+            flowLayoutPanelListings.Controls.Clear();
+
+            // Add car listing panels to the flow layout panel
+            foreach (var car in carListings)
+            {
+                // Create a car listing panel
+                Panel carPanel = CreateCarListingPanel(car);
+                flowLayoutPanelListings.Controls.Add(carPanel);
+            }
+        }
+
+        private Panel CreateCarListingPanel(CarListing car)
+        {
+            // Main panel
+            Panel panel = new Panel();
+            panel.Size = new Size(250, 310);
+            panel.BorderStyle = BorderStyle.FixedSingle;
+            panel.Margin = new Padding(10);
+            panel.BackColor = Color.White;
+
+            // Car image
+            PictureBox pictureBox = new PictureBox();
+            pictureBox.Size = new Size(230, 150);
+            pictureBox.Location = new Point(10, 10);
+            pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+            try
+            {
+                pictureBox.Image = Image.FromFile(Path.Combine("Images", car.ImagePath));
+            }
+            catch
+            {
+                // Use placeholder if image not found
+                pictureBox.BackColor = Color.LightGray;
+            }
+
+            // Car title (Make and Model)
+            Label lblTitle = new Label();
+            lblTitle.AutoSize = false;
+            lblTitle.Size = new Size(230, 20);
+            lblTitle.Location = new Point(10, 170);
+            lblTitle.Font = new Font("Segoe UI", 9, FontStyle.Bold);
+            lblTitle.Text = $"{car.Make} {car.Model} - {car.Year}";
+
+            // Car description
+            Label lblDescription = new Label();
+            lblDescription.AutoSize = false;
+            lblDescription.Size = new Size(230, 60);
+            lblDescription.Location = new Point(10, 190);
+            lblDescription.Font = new Font("Segoe UI", 8);
+            lblDescription.Text = car.Description;
+
+            // Price label
+            Label lblPrice = new Label();
+            lblPrice.AutoSize = false;
+            lblPrice.Size = new Size(100, 20);
+            lblPrice.Location = new Point(10, 255);
+            lblPrice.Font = new Font("Segoe UI", 9, FontStyle.Bold);
+            lblPrice.Text = $"Rs {car.PricePerDay}/day";
+
+            // View deal button
+            Button btnViewDeal = new Button();
+            btnViewDeal.Size = new Size(80, 30);
+            btnViewDeal.Location = new Point(160, 250);
+            btnViewDeal.Text = "View Deal";
+            btnViewDeal.BackColor = Color.FromArgb(30, 85, 110);
+            btnViewDeal.ForeColor = Color.White;
+            btnViewDeal.FlatStyle = FlatStyle.Flat;
+            btnViewDeal.Tag = car.Id;
+            btnViewDeal.Click += BtnViewDeal_Click;
+
+            // Add controls to panel
+            panel.Controls.Add(pictureBox);
+            panel.Controls.Add(lblTitle);
+            panel.Controls.Add(lblDescription);
+            panel.Controls.Add(lblPrice);
+            panel.Controls.Add(btnViewDeal);
+
+            return panel;
+        }
+
+        private void BtnViewDeal_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            int carId = (int)btn.Tag;
+
+            // Find the car
+            CarListing selectedCar = carListings.FirstOrDefault(c => c.Id == carId);
+
+            if (selectedCar != null)
+            {
+                // Open the booking form or show details
+                MessageBox.Show($"You selected: {selectedCar.Make} {selectedCar.Model}\n" +
+                               $"Price: Rs {selectedCar.PricePerDay}/day",
+                               "View Deal",
+                               MessageBoxButtons.OK,
+                               MessageBoxIcon.Information);
+
+                // In a real application, you would open a booking form here
+                // BookingForm bookingForm = new BookingForm(selectedCar);
+                // bookingForm.ShowDialog();
+            }
+        }
+
+        private void btnBrowseListings_Click(object sender, EventArgs e)
+        {
+            // Already on the listings page, could refresh or apply filters
+            PopulateCarListings();
+        }
+
+        private void btnListCar_Click(object sender, EventArgs e)
+        {
+            // Open form to list a new car
+            MessageBox.Show("List a Car functionality would open a form to add a new listing.",
+                           "List a Car",
+                           MessageBoxButtons.OK,
+                           MessageBoxIcon.Information);
+
+            // In a real application, you would open a form here
+            // ListCarForm listCarForm = new ListCarForm();
+            // if (listCarForm.ShowDialog() == DialogResult.OK)
+            // {
+            //     LoadCarListings();
+            //     PopulateCarListings();
+            // }
+        }
+
+        private void btnManageBooking_Click(object sender, EventArgs e)
+        {
+            // Open form to manage bookings
+            MessageBox.Show("Manage Booking functionality would open a form to view and manage bookings.",
+                           "Manage Bookings",
+                           MessageBoxButtons.OK,
+                           MessageBoxIcon.Information);
+
+            // In a real application, you would open a form here
+            // ManageBookingsForm manageBookingsForm = new ManageBookingsForm();
+            // manageBookingsForm.ShowDialog();
+        }
+
+        private void btnOptions_Click(object sender, EventArgs e)
+        {
+            // Open options/settings
+            MessageBox.Show("Options functionality would open settings for the application.",
+                           "Options",
+                           MessageBoxButtons.OK,
+                           MessageBoxIcon.Information);
+
+            // In a real application, you would open a form here
+            // OptionsForm optionsForm = new OptionsForm();
+            // optionsForm.ShowDialog();
+        }
+
+        private void textBoxSearch_TextChanged(object sender, EventArgs e)
+        {
+            // Filter cars based on search text
+            string searchText = textBoxSearch.Text.ToLower();
+
+            if (string.IsNullOrWhiteSpace(searchText))
+            {
+                PopulateCarListings(); // Show all if search is empty
+                return;
+            }
+
+            var filteredCars = carListings.Where(c =>
+                c.Make.ToLower().Contains(searchText) ||
+                c.Model.ToLower().Contains(searchText) ||
+                c.Description.ToLower().Contains(searchText)).ToList();
+
+            // Clear and repopulate with filtered results
+            flowLayoutPanelListings.Controls.Clear();
+            foreach (var car in filteredCars)
+            {
+                Panel carPanel = CreateCarListingPanel(car);
+                flowLayoutPanelListings.Controls.Add(carPanel);
+            }
+        }
+    }
+
+    public class CarListing
+    {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Description { get; set; }
+        public decimal PricePerDay { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
