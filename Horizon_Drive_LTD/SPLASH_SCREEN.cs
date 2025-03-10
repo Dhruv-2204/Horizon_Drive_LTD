94eb9b2c706b18bb3802fbbfe1f7cdef7081f2c8
ATPL810 <aayush810lochun@gmail.com>
2025-03-10 05:15:08 +0400

    committing

---
+++ Horizon_Drive_LTD/SPLASH_SCREEN.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media; 
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace splashscreen
+{
+    public partial class SPLASH_SCREEN : Form
+    {
+        public SPLASH_SCREEN()
+        {
+            InitializeComponent();
+            PlaySound();
+
+        }
+        private void PlaySound()
+        {
+            try
+            {
+                // Locate the Horizon_Drive_LTD directory dynamically
+                DirectoryInfo currentDir = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory);
+
+                while (currentDir != null && currentDir.Name != "Horizon_Drive_LTD")
+                {
+                    currentDir = currentDir.Parent;
+                }
+
+                if (currentDir == null)
+                {
+                    MessageBox.Show("Error: Could not locate Horizon_Drive_LTD directory.", "Directory Error",
+                                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                // Construct the path to the sound file directly inside Horizon_Drive_LTD
+                string soundPath = Path.Combine(currentDir.FullName, "Sound\\Engine_Start-Slash_Screen.wav");
+
+                // Debugging - Print the path to verify correctness
+                Console.WriteLine($"Sound Path: {soundPath}");
+
+                // Check if the sound file exists before playing
+                if (!File.Exists(soundPath))
+                {
+                    MessageBox.Show($"Error: Sound file not found!\nPath: {soundPath}", "File Not Found",
+                                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                // Create SoundPlayer instance and play the sound
+                using (SoundPlayer player = new SoundPlayer(soundPath))
+                {
+                    player.Play(); // Play once (use player.PlayLooping() for continuous play)
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error playing sound: {ex.Message}", "Playback Error",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            panel2.Width += 3;
+            if (panel2.Width >= 585)
+            {
+                timer1.Stop();
+                OpensignUp();
+
+
+            }
+        }
+
+        private void OpensignUp()
+        {
+            SignUp signup = new SignUp();
+            signup.Show();
+            this.Hide(); // Hide splash screen instead of closing
+        }
+
+       
+    }
+}
