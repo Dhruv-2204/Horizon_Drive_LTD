abb7215c6155bf6f30318e186a74ed75d0c33910
RumaisaG <rumaisa.g28@gmail.com>
2025-03-24 23:02:08 +0400

    adding the hash table class

--- Horizon_Drive_LTD/HashTable.cs
+++ Horizon_Drive_LTD/HashTable.cs
@@ -6,66 +6,185 @@ using System.Threading.Tasks;
 
 namespace Horizon_Drive_LTD
 {
-    public class HashTable<TKey, TValue>
+    class HashTable<TKey, TValue>
     {
-        private const int DefaultCapacity = 16;
-        private LinkedList<TKey, TValue>[] _buckets;
+        private int _capacity;
+        private int _size;
+        private float _loadFactor;
+        private LinkedList<KeyValuePair<TKey, TValue>>[] _buckets;
 
-        public HashTable()
+        public HashTable(int capacity, float loadFactor = 0.75f)
         {
-            _buckets = new LinkedList<TKey, TValue>[DefaultCapacity];
+            if (capacity <= 0)
+                throw new ArgumentException("Capacity must be greater than 0.");
+            if (loadFactor <= 0 || loadFactor >= 1)
+                throw new ArgumentException("Load factor must be between 0 and 1.");
+
+            _capacity = GetNextPrime(capacity);
+            _loadFactor = loadFactor;
+            _size = 0;
+            _buckets = new LinkedList<KeyValuePair<TKey, TValue>>[_capacity];
         }
 
-        // Fibonacci-based hash function
-        private int FibonacciHash(TKey key)
+        private int GetHash(TKey key)
         {
-            const double goldenRatio = 1.618033988749895; // (sqrt(5) + 1) / 2
-            int hashCode = key.GetHashCode();
-            double product = hashCode * goldenRatio;
+            int hash = key.GetHashCode();
+            double product = hash * 0.618033988749895; // Fibonacci hashing
             double fractionalPart = product - Math.Floor(product);
-            return (int)(_buckets.Length * fractionalPart);
+            return (int)(_capacity * fractionalPart);
         }
 
-        private int GetBucketIndex(TKey key)
+        public void Insert(TKey key, TValue value)
         {
-            return FibonacciHash(key) % _buckets.Length;
-        }
+            if (ShouldResize())
+                Resize();
 
-        public void Add(TKey key, TValue value)
-        {
-            int bucketIndex = GetBucketIndex(key);
+            int index = GetHash(key);
 
-            if (_buckets[bucketIndex] == null)
+            if (_buckets[index] == null)
             {
-                _buckets[bucketIndex] = new LinkedList<TKey, TValue>();
+                _buckets[index] = new LinkedList<KeyValuePair<TKey, TValue>>();
             }
 
-            _buckets[bucketIndex].Add(key, value);
-        }
-
-        public bool TryGetValue(TKey key, out TValue value)
-        {
-            int bucketIndex = GetBucketIndex(key);
-
-            if (_buckets[bucketIndex] != null)
+            var bucket = _buckets[index];
+            var existingNode = bucket.FirstOrDefault(node => node.Key.Equals(key));
+            if (!EqualityComparer<TKey>.Default.Equals(existingNode.Key, default) && existingNode.Key.Equals(key))
             {
-                return _buckets[bucketIndex].TryGetValue(key, out value);
+                // Remove the existing key-value pair
+                bucket.Remove(existingNode);
+
+                // Add a new key-value pair with the updated value
+                bucket.AddLast(new KeyValuePair<TKey, TValue>(key, value));   // It was immutable before that (struct)
+            }
+            else
+            {
+                // Add a new key-value pair
+                bucket.AddLast(new KeyValuePair<TKey, TValue>(key, value));
+                _size++;
             }
 
-            value = default;
-            return false;
+        }
+
+        public TValue Search(TKey key)
+        {
+            int index = GetHash(key);
+
+            if (_buckets[index] != null)
+            {
+                foreach (var kvp in _buckets[index])
+                {
+                    if (kvp.Key.Equals(key))
+                    {
+                        return kvp.Value; // Return the value if the key is found
+                    }
+                }
+            }
+
+            return default(TValue); // Key not found
         }
 
         public bool Remove(TKey key)
         {
-            int bucketIndex = GetBucketIndex(key);
+            int index = GetHash(key);
 
-            if (_buckets[bucketIndex] != null)
+            if (_buckets[index] != null)
             {
-                return _buckets[bucketIndex].Remove(key);
+                var node = _buckets[index].FirstOrDefault(n => n.Key.Equals(key));
+                if (node.Key != null && node.Key.Equals(key))
+                {
+                    _buckets[index].Remove(node);
+                    _size--;
+                    return true;
+                }
             }
 
-            return false;
+            return false; // Key not found
+        }
+
+        //public void GetAllItems()
+        //{
+        //    for (int i = 0; i < _capacity; i++)
+        //    {
+        //        Console.Write($"Index {i}: ");
+        //        if (_buckets[i] != null)
+        //        {
+        //            foreach (var kvp in _buckets[i])
+        //            {
+        //                Console.Write($"[{kvp.Key}: {kvp.Value}] -> ");
+        //            }
+        //            Console.WriteLine("null");
+        //        }
+        //        else
+        //        {
+        //            Console.WriteLine("null");
+        //        }
+        //    }
+        //}
+
+        public IEnumerable<KeyValuePair<TKey, TValue>> GetAllItems()
+        {
+            for (int i = 0; i < _capacity; i++)
+            {
+                if (_buckets[i] != null)
+                {
+                    foreach (var kvp in _buckets[i])
+                    {
+                        yield return kvp;
+                    }
+                }
+            }
+        }
+
+        private void Resize()
+        {
+            int newCapacity = GetNextPrime(_capacity * 2);
+            var newBuckets = new LinkedList<KeyValuePair<TKey, TValue>>[newCapacity];
+
+            for (int i = 0; i < _capacity; i++)
+            {
+                if (_buckets[i] != null)
+                {
+                    foreach (var kvp in _buckets[i])
+                    {
+                        int newIndex = GetHash(kvp.Key);
+                        if (newBuckets[newIndex] == null)
+                        {
+                            newBuckets[newIndex] = new LinkedList<KeyValuePair<TKey, TValue>>();
+                        }
+                        newBuckets[newIndex].AddLast(kvp);
+                    }
+                }
+            }
+
+            _buckets = newBuckets;
+            _capacity = newCapacity;
+        }
+
+        private bool ShouldResize()
+        {
+            return (float)_size / _capacity >= _loadFactor;
+        }
+
+        private int GetNextPrime(int number)
+        {
+            while (true)
+            {
+                if (IsPrime(number))
+                    return number;
+                number++;
+            }
+        }
+
+        private bool IsPrime(int number)
+        {
+            if (number < 2)
+                return false;
+            for (int i = 2; i * i <= number; i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+            return true;
         }
     }
 }
