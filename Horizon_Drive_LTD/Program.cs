c3e9aab948e84f4581fd217f40590948c2d24158
RumaisaG <rumaisa.g28@gmail.com>
2025-04-10 22:48:22 +0400

    added folders for testing and media

--- Horizon_Drive_LTD/Program.cs
+++ Horizon_Drive_LTD/Program.cs
@@ -20,15 +20,12 @@ namespace splashscreen
         static void Main()
         {
 
+         // Enable high-DPI awareness
+         Application.SetHighDpiMode(HighDpiMode.SystemAware);
+         Application.EnableVisualStyles();
+         Application.SetCompatibleTextRenderingDefault(false);
+         Application.Run(new SPLASH_SCREEN());
 
-          
-
-
-            // Enable high-DPI awareness
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new SPLASH_SCREEN());
         }
     }
 }
