45160a8f35fc4cc482f412e856a5bcfa4095a700
Dhruv Ramachundrah <DR717@live.mdx.ac.uk>
2025-04-14 16:54:04 +0400

    Was able to add reference to HorizonDriveLTD project and fixed target framework

---
+++ xUnitTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("xUnitTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("xUnitTesting")]
+[assembly: AssemblyCopyright("Copyright © HP 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("13718eda-b4bf-4ef3-952b-4e5489af88bb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ xUnitTesting1/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿//using Xunit;
+
+//namespace xUnitTesting1
+//{
+//    public class UnitTest1
+//    {
+//        [Fact]
+//        public void Test1()
+//        {
+
+//        }
+//    }
+//}
+
+using Xunit;
+using Horizon_Drive_LTD;
+using System.Collections.Generic;
+using Horizon_Drive_LTD.DataStructure;
+
+namespace xUnitTesting
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void InsertAndSearch_ReturnsCorrectValue()
+        {
+            // Arrange
+            var table = new HashTable<string, string>(5);
+
+            // Act
+            table.Insert("username", "tisha123");
+            var result = table.Search("username");
+
+            // Assert
+            Assert.Equal("tisha123", result);
+        }
+    }
+}
